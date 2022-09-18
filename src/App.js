fae07868f28f96d70a64f282e0883a0e72950a94
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-09-17 22:35:38 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Stories from "./Stories"
+import NavBar from "./NavBar"
 
 export default function App (){
     return(
         <>
+            <NavBar />
             <Stories />
         </>
     )
---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+export default function NavBar(){
+    return(
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <div className="separador"></div>
+                    <img src='/img/logo.png'/>
+                </div>
+
+                <div className="pesquisa">
+                    <input></input>
+                </div>
+
+                <div className="icones">
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                    <ion-icon name="compass-outline"></ion-icon>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="person-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    )
+}
